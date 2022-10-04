9c8fde98bf3ff25ab77d2969433393fd32914545
Roshan Singh <roshan@bit01.tech>
2022-10-04 09:34:51 +0530

    refactoring design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:haul27/constants/app_colors.dart';
-import 'package:haul27/pages/login_page.dart';
+
+import 'pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +10,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Haul27',
       theme: ThemeData(
         primaryColor: kAscentLightPrimary,
       ),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,59 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:haul27/constants/app_textstyles.dart';
-import 'package:rive/rive.dart';
-
-import '../widgets/haul_button.dart';
-import '../widgets/haul_text_button.dart';
-import 'home_navigator_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 32.0),
-                  child: Text(
-                    'haul27',
-                    style: kHaul27,
-                  ),
-                ),
-                HaultTextButton(
-                  child: const Text('Sign up'),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            const AspectRatio(
-              aspectRatio: 1,
-              child: RiveAnimation.asset(
-                'assets/cycle.riv',
-              ),
-            ),
-            Text(
-              'New way of touring with friends',
-              style: kTextLargeTextStyle,
-            )
-          ]),
-          SizedBox(
-            width: double.maxFinite,
-            child: HaulButton(
-              child: const Text('Sign in with Email'),
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const HomeNavigatorPage())),
-            ),
-          ),
-        ],
-      )),
-    );
+    return Scaffold();
   }
 }
